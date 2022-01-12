dc934ddc36800f0d2fb89dfaf197610c42093dcf
Julien Richard <33219171+jujuck@users.noreply.github.com>
2022-01-12 14:32:26 +0100

    Update users.sql

--- sql/users.sql
+++ sql/users.sql
@@ -6,7 +6,7 @@ CREATE TABLE users(
 );
 
 INSERT INTO users(uuid_users,email,hashedpassword) 
-  VALUES ('$2b$10$DpDzpIthDFMGK1E7fMuexulyL6KiVzFAirevhuyC5EIf5xVmWQJQW','toto@wildcodeschool.com','$2b$10$DpDzpIthDFMGK1E7fMuexulyL6KiVzFAirevhuyC5EIf5xVmWQJQW');
+  VALUES ('caa9d470-74d8-40dd-bc65-90c74d26632f','toto@wildcodeschool.com','$2b$10$DpDzpIthDFMGK1E7fMuexulyL6KiVzFAirevhuyC5EIf5xVmWQJQW');
 
 
 /** DO NOT PASTE IT mdp => MDPwild-22 */
