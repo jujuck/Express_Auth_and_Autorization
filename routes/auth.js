e4c4e4fdf160024ce0a4ea0582c903f09b451047
Julien ichard WCS <julien.y.richard@gmail.com>
2022-06-01 17:10:53 +0200

    Creating the auht file with a new router and implementing it in the index.js file

---
+++ routes/auth.js
@@ -0,0 +1,3 @@
+const authRouter = require('express').Router();
+
+module.exports = authRouter;
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,9 @@
 const beersRouter = require('./beers');
+const authRouter = require('./auth');
 
 const setUpRoutes = (app) => {
-  app.use('/beers', beersRouter)
+  app.use('/beers', beersRouter),
+  app.use('/auth', authRouter)
 };
 
 module.exports = {
